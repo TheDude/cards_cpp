5fd79862de2fa8a2898b43eecdb7ad6cea300026
TheDude <t.ben.creighton@gmail.com>
2026-08-15 11:20:31 -0500

    fixed conan build

--- conanfile.py
+++ conanfile.py
@@ -2,11 +2,21 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class Deck(ConanFile):
+    options = {"unit_tests": [True, False]}
+    default_options = {"unit_tests": False}
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
+    # in a real project, you would probably have real dependencies
+    # this method is just here to document where those would go
     def requirements(self):
-        self.requires("gtest/1.15.0")
+        pass
+
+    def build_requirements(self):
+        # self.test_requires is for testing only requirements (e.g. gtest)
+        # use self.requires for normal dependencies.
+        if self.options.unit_tests:
+            self.test_requires("gtest/1.15.0")
 
     def layout(self):
         cmake_layout(self)
@@ -15,3 +25,5 @@ class Deck(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+        if self.options.unit_tests:
+            cmake.test()
